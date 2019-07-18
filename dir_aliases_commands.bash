5c85afaad7da6ece5051ca0bb6f52337d324ce42
Allen Barker <Allen.L.Barker@gmail.com>
2019-07-17 20:20:05 -0400

    first commit

---
+++ dir_aliases_commands.bash
@@ -0,0 +1,119 @@
+# This file must be sourced from Bash; it will not work if executed as a normal script.
+if [ "${BASH_SOURCE[0]}" -ef "$0" ]; then
+    echo "Usage: source dir_aliases_commands.bash"
+    exit 1
+fi
+
+# This file contains the definition of the dir-alias command, along with the
+# commands show-dir-aliases and aliases to display alias information.
+
+declare -A dir_aliases_map # Associative array holding dir-alias documentation strings.
+
+dir-alias() {
+   usage="Usage: dir-alias <shortcut_name> [<directory_path>] [-c <command_to_run>]"
+   #
+   # This command creates both a shell function that changes to the directory
+   # and a shell variable which contains the directory name (so you can use $
+   # access the directory path to use in another command).  An optional command
+   # to be executed can be passed as a string after a `-c` flag at the end.  It
+   # runs after any directory change.
+   #
+   # Any existing shell function or ordinary alias is deleted and a new shell
+   # function is created.
+   #
+   # Previously-defined dir-alias aliases can be used in the definition of later
+   # ones.  The definitions are exported so scripts can also use them.
+   local hide_indicator cmd dirname alias_name alias_type
+   if [ "$1" == "" ] || [ "$2" == "" ]; then
+      echo $usage
+      echo "At least two arguments must be passed to dir-alias."
+      return 1
+   fi
+
+   # Get the alias name.
+   alias_name="$1"
+   unset -f $alias_name # A new shell function will be defined; delete old one.
+   unalias $alias_name 2>/dev/null # Remove any ordinary alias of the same name.
+   alias_type=$(type -t $alias_name)
+   if [ "$alias_type" != "function" ] && [ "$alias_type" != "alias" ] && [ "$alias_type" != "" ]; then
+      hide_indicator="*"
+   fi
+
+   # Get the directory name.
+   shift
+   if [ "$1" == "-c" ]; then
+      dirname=""
+   else
+      dirname="$1"
+      shift
+   fi
+
+   # Get the command.
+   cmd=""
+   if [ "$1" != "" ]; then
+      if [ "$1" != "-c" ]; then
+         echo $usage
+         echo "A -c flag is required before the command string passed to dir-alias ('$alias_name')."
+         return 1
+      elif [ "$2" == "" ]; then
+         echo $usage
+         echo "An empty command string was passed to the -c argument to dir-alias ('$alias_name')."
+         return 1
+      elif [ "$3" != "" ]; then
+         echo $usage
+         echo "Too many arguments to dir-alias ('$alias_name').  Any command passed in must be quoted."
+         return 1
+      fi
+      cmd="$2"
+   fi
+
+   # Define and export the shell function.
+   if [ "$dirname" == "" ]; then
+      eval "$alias_name() { $cmd; }" # Define shell fun with given name.
+      dir_aliases_map[$alias_name]="${alias_name}${hide_indicator} -c \"$cmd\""
+   elif [ "$cmd" == "" ]; then
+      eval "$alias_name() { cd $dirname; }" # Define shell fun with the given name.
+      dir_aliases_map[$alias_name]="${alias_name}${hide_indicator} -- $dirname"
+   else
+      eval "$alias_name() { cd $dirname; $cmd; }" # Define shell fun with given name.
+      dir_aliases_map[$alias_name]="${alias_name}${hide_indicator} -- $dirname -c \"$cmd\""
+   fi
+   export -f "$alias_name" # Export the function.
+
+   # Define and export the shell variable.
+   eval export $alias_name="$dirname"
+}
+
+show-dir-aliases() {
+   # Usage: show-dir-aliases
+   #
+   # Sort and show the list of directory aliases.  Any which shadow existing
+   # commands (other than shell functions and ordinary aliases) are also marked
+   # with a asterisk after the alias name.
+
+   local i sorted
+   # Get a sorted array of all the values in dir_aliases_map.
+   IFS=$'\n' sorted=($(sort <<<"${dir_aliases_map[*]}")) # Sort the strings.
+
+   for i in "${sorted[@]}"
+   do
+      echo $i
+   done
+}
+
+aliases() {
+   # Usage: aliases
+   #
+   # Prints out a nicer looking version of the plain alias command followed
+   # by the aliases set with dir-aliases.  The show-dir-aliases command is used to
+   # display the latter.
+
+   echo "Regular aliases:"
+   echo "================"
+   alias | sed 's/^alias //' | sed 's/\=/ -- /'
+   echo
+   echo "Directory aliases:"
+   echo "=================="
+   show-dir-aliases
+}
+
